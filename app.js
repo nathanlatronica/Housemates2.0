4cb45fd7c90b3baf5211d89f8c330290def32aa7
Nathan Latronica <nathan.latronica@gmail.com>
2022-02-08 16:35:06 -0800

    initialize

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
+const port = process.env.PORT || 3000
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static("public")); //folder for images, css, js
+app.use('/public', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', async function (req, res) {
+
+
+    res.render("homePage");
+});
